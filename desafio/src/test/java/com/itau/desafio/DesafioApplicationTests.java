bd7b705b6657a12704d8e0145a5d6a700d71e51a
vlmdr <vinicius.rio@edge.ufal.br>
2025-02-13 19:53:12 -0300

    feat(test): Add firsts tests case

--- desafio/src/test/java/com/itau/desafio/DesafioApplicationTests.java
+++ desafio/src/test/java/com/itau/desafio/DesafioApplicationTests.java
@@ -2,12 +2,36 @@ package com.itau.desafio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import java.time.OffsetDateTime;
 
 @SpringBootTest
 class DesafioApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void shouldValidDate() {
+		TransactionService transactionService = new TransactionService();
+		Transaction transaction = new Transaction(100.0, OffsetDateTime.now().plusDays(1));
+
+		assertThrows(
+				IllegalArgumentException.class,
+				() -> transactionService.addTransaction(transaction)
+		);
+
+	}
+
+	@Test
+	public void shouldValidValue() {
+		TransactionService transactionService = new TransactionService();
+		Transaction transaction = new Transaction(-1.0, OffsetDateTime.now());
+
+		assertThrows(
+				IllegalArgumentException.class,
+				() -> transactionService.addTransaction(transaction)
+		);
+
 	}
 
 }
